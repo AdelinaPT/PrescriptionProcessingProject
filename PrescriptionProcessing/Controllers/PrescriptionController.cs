891cad412738804ebe2ea4bed4b77c9733e3eb6a
Adelina Platon <Adelina Platon@DESKTOP-ANK96PO>
2022-11-13 10:53:33 +0200

    creat Controllere

---
+++ PrescriptionProcessing/Controllers/PrescriptionController.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PrescriptionProcessing.Controllers
+{
+    public class PrescriptionController : Controller
+    {
+        // GET: PrescriptionController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: PrescriptionController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: PrescriptionController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: PrescriptionController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: PrescriptionController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: PrescriptionController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: PrescriptionController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: PrescriptionController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
